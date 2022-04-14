3d488f3cf28000e1bb7976e4e810d60ad277000b
dmikhaylov <d.y.mikhayov@yandex.ru>
2022-04-14 23:45:13 +0400

    Реализован правильный переход между страницами

--- pages/login_page.py
+++ pages/login_page.py
@@ -10,7 +10,7 @@ class LoginPage(BasePage):
 
     def should_be_login_url(self):
         # реализуйте проверку на корректный url адрес
-        assert "login" in self.browser.current_url(), "Wrong login url"
+        assert "login" in self.browser.current_url, "Wrong login url"
 
     def should_be_login_form(self):
         # реализуйте проверку, что есть форма логина
